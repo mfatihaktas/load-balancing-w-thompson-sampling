88f2207639b2c4e020c8f823179e5fb45d3b2e96
Mehmet Aktas <mehmet@overjet.ai>
2022-12-03 23:03:50 -0500

    Finished first draft of ThompsonSampling_slidingWin_Gaussian

--- src/agent/ts.py
+++ src/agent/ts.py
@@ -17,7 +17,7 @@ class ThompsonSampling_slidingWin(agent.SchingAgent):
 
     def __repr__(self):
         return (
-            "ThompsonSampling_slidingWin_Gaussian( \n"
+            "ThompsonSampling_slidingWin( \n"
             f"{super().__repr__()} \n"
             f"\t win_len= {self.win_len} \n"
             ")"
@@ -41,13 +41,12 @@ class ThompsonSampling_slidingWin(agent.SchingAgent):
 
         # Choose the node with min cost sample
         node_id_w_min_sample, min_sample = None, float("Inf")
-        for node_id, cost_list in node_id_to_costs_map.items():
-            mean = numpy.mean(cost_list) if len(cost_list) else 0
-            stdev = numpy.std(cost_list) if len(cost_list) else 1
+        for node_id, cost_queue in node_id_to_costs_map.items():
+            mean = numpy.mean(cost_queue) if len(cost_queue) else 0
+            stdev = numpy.std(cost_queue) if len(cost_queue) else 1
             check(stdev >= 0, "Stdev cannot be negative")
             if stdev == 0:
                 stdev = 1
-
 
             s = random_variable.TruncatedNormal(mu=mean, sigma=stdev).sample()
             if s < min_sample:
@@ -58,53 +57,42 @@ class ThompsonSampling_slidingWin(agent.SchingAgent):
         return node_id_w_min_sample
 
 
-"""
-class ThompsonSampling_slidingWin_Gaussian(agent.SchingAgent):
+class ThompsonSampling_slidingWinForEachNode(agent.SchingAgent):
     def __init__(self, node_id_list: list[str], win_len: int):
         super().__init__(node_id_list=node_id_list)
 
         self.win_len = win_len
 
-        self.node_id_cost_queue = collections.deque(maxlen=win_len)
+        self.node_id_to_cost_queue_map = {node_id: collections.deque(maxlen=win_len) for node_id in node_id_list}
 
     def __repr__(self):
         return (
-            "ThompsonSampling_slidingWin_Gaussian( \n"
+            "ThompsonSampling_slidingWinForEachNode( \n"
             f"{super().__repr__()} \n"
             f"\t win_len= {self.win_len} \n"
             ")"
         )
 
     def record_cost(self, node_id: str, cost: float):
-        self.node_id_cost_queue.append((node_id, cost))
+        self.node_id_to_cost_queue_map[node_id].append(cost)
         log(DEBUG, "recorded", node_id=node_id, cost=cost)
 
     def node_to_schedule(self) -> str:
-        # Construct `node_id_to_costs_map`
-        node_id_to_costs_map = collections.defaultdict(list)
-        for (node_id, cost) in self.node_id_cost_queue:
-            node_id_to_costs_map[node_id].append(cost)
-
-        for node_id in self.node_id_list:
-            if node_id not in node_id_to_costs_map:
-                node_id_to_costs_map[node_id].append(0)
-
-        log(DEBUG, "", node_id_to_costs_map=node_id_to_costs_map)
+        log(DEBUG, "", node_id_to_cost_queue_map=self.node_id_to_cost_queue_map)
 
         # Choose the node with min cost sample
         node_id_w_min_sample, min_sample = None, float("Inf")
-        for node_id, cost_list in node_id_to_costs_map.items():
-            mean = numpy.mean(cost_list) if len(cost_list) else 0
-            stdev = numpy.std(cost_list) if len(cost_list) else 1
+        for node_id, cost_queue in self.node_id_to_cost_queue_map.items():
+            mean = numpy.mean(cost_queue) if len(cost_queue) else 0
+            stdev = numpy.std(cost_queue) if len(cost_queue) else 1
             check(stdev >= 0, "Stdev cannot be negative")
             if stdev == 0:
                 stdev = 1
 
-            s = numpy.random.normal(loc=mean, scale=stdev)
+            s = random_variable.TruncatedNormal(mu=mean, sigma=stdev).sample()
             if s < min_sample:
                 min_sample = s
                 node_id_w_min_sample = node_id
                 # log(DEBUG, "s < min_sample", s=s, min_sample=min_sample, node_id_w_min_sample=node_id_w_min_sample)
 
         return node_id_w_min_sample
-"""
