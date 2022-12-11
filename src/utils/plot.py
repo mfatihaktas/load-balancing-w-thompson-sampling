31333a2edbe913fd9551de9f92dc61a08eab8ccc
Mehmet Aktas <mehmet@overjet.ai>
2022-12-10 20:46:30 -0500

    Moved to _cycle

--- src/utils/plot.py
+++ src/utils/plot.py
@@ -18,12 +18,12 @@ NICE_RED = "#ff9999"
 NICE_GREEN = "#99ff99"
 NICE_ORANGE = "#ffcc99"
 
-nice_color_c = itertools.cycle((NICE_BLUE, NICE_RED, NICE_ORANGE, NICE_GREEN))
-dark_color_c = itertools.cycle(("green", "purple", "blue", "magenta", "purple", "gray", "brown", "turquoise", "gold", "olive", "silver", "rosybrown", "plum", "goldenrod", "lightsteelblue", "lightpink", "orange", "darkgray", "orangered"))
-# dark_color_c = itertools.cycle(("magenta", "purple", "gray", "brown", "turquoise", "gold", "olive", "silver", "rosybrown", "plum", "goldenrod", "lightsteelblue", "lightpink", "orange", "darkgray", "orangered"))
-light_color_c = itertools.cycle(("silver", "rosybrown", "plum", "lightsteelblue", "lightpink", "orange", "turquoise"))
-linestyle_c = itertools.cycle(("-", "--", ":", "-.") )
-marker_c = itertools.cycle(("o", "v", "^", "p", "d", "<", ">", "h", "H", "*", "s", "1" , "2", "3", "4") )
+nice_color_cycle = itertools.cycle((NICE_BLUE, NICE_RED, NICE_ORANGE, NICE_GREEN))
+dark_color_cycle = itertools.cycle(("green", "purple", "blue", "magenta", "purple", "gray", "brown", "turquoise", "gold", "olive", "silver", "rosybrown", "plum", "goldenrod", "lightsteelblue", "lightpink", "orange", "darkgray", "orangered"))
+# dark_color_cycle = itertools.cycle(("magenta", "purple", "gray", "brown", "turquoise", "gold", "olive", "silver", "rosybrown", "plum", "goldenrod", "lightsteelblue", "lightpink", "orange", "darkgray", "orangered"))
+light_color_cycle = itertools.cycle(("silver", "rosybrown", "plum", "lightsteelblue", "lightpink", "orange", "turquoise"))
+linestyle_cycle = itertools.cycle(("-", "--", ":", "-.") )
+marker_cycle = itertools.cycle(("o", "v", "^", "p", "d", "<", ">", "h", "H", "*", "s", "1" , "2", "3", "4") )
 skinny_marker_l = ["x", "+", "1", "2", "3", "4"]
 
 mew, ms = 1, 2 # 3, 5
