1815596f46018371c36a78d001100a15e4b2910a
Mehmet Aktas <mehmet@overjet.ai>
2022-12-14 14:12:58 -0500

    Added AssignToNoisyLeastWorkLeft

--- src/agent/optimal.py
+++ src/agent/optimal.py
@@ -1,5 +1,8 @@
 from src.agent import agent
+from src.prob import random_variable
 from src.sys import node
+
+from src.utils.debug import *
 
 
 class AssignToLeastWorkLeft(agent.SchingAgent):
@@ -18,6 +21,37 @@ class AssignToLeastWorkLeft(agent.SchingAgent):
         least_work = float("Inf")
         for node in self.node_list:
             work_left = node.work_left()
+
+            if work_left < least_work:
+                least_work = work_left
+                node_w_least_work = node
+
+        return node_w_least_work._id
+
+
+class AssignToNoisyLeastWorkLeft(AssignToLeastWorkLeft):
+    def __init__(self, node_list: list[node.Node], noise_rv: random_variable.RandomVariable):
+        super().__init__(node_list=node_list)
+        self.noise_rv = noise_rv
+
+        check(self.noise_rv.min_value > 0 and self.noise_rv.max_value > 0,
+              f"Improper noise_rv= {noise_rv}")
+
+    def __repr__(self):
+        return (
+            "AssignToNoisyLeastWorkLeft( \n"
+            f"\t num_nodes= {len(self.node_list)} \n"
+            f"\t noise_rv= {self.noise_rv} \n"
+            ")"
+        )
+
+    def node_id_to_assign(self) -> str:
+        node_w_least_work = None
+        least_work = float("Inf")
+        for node in self.node_list:
+            noise = self.noise_rv.sample()
+            log(DEBUG, f"Sampled noise= {noise}", node=node)
+            work_left = node.work_left() * noise
 
             if work_left < least_work:
                 least_work = work_left
