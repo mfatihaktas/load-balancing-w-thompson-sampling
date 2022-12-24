2ddab949480200917ad28611580bd8f6bebc963d
Mehmet Aktas <mehmet@overjet.ai>
2022-12-24 15:19:45 -0500

    Deleted run_tests.sh

--- run_tests.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-
-PYTEST="pytest --color=yes --verbose --showlocals"
-
-# ${PYTEST} "tests/test_ts_sliding_win.py::test_AssignWithThompsonSampling_slidingWin_vs_slidingWinForEachNode"
-${PYTEST} "tests/test_optimal_vs_ts.py::test_optimal_vs_ts"
